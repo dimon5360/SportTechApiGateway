82d3575860a9918de5c42d7a09a6635fe4e99cfb
Dmitry <dimon5360@gmail.com>
2023-11-14 22:45:12 +0300

    replace source code

--- main.go
+++ main.go
@@ -7,12 +7,18 @@ import (
 	server "main/core/web"
 )
 
+/// TODO:
+/// 1. write and compile protobuf
+/// 2. include grpc
+/// 3. transfer queries and process responses
+/// 4. connect to kafka
+
 func main() {
 
 	env := utils.Init()
-	env.Load("configs/app.env")
+	env.Load("env/app.env")
 
-	fmt.Println("Application v." + env.Value("VERSION_APP"))
+	fmt.Println("Core service v." + env.Value("VERSION_APP"))
 
 	server.InitServer(router.InitRouter(env.Value("HOST"))).Run()
 }
